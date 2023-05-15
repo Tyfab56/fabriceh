ae42ccd520eba0816f73b3424a528462e9dd7fc8
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-05-15 15:17:49 +0400

    Body3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,22 +24,20 @@
 			<div class="row">
 				
                 <div class="hfg-slot center">
-                    <div class="builder-item desktop-left"><div class="item--inner builder-item--logo" >
-                        <div class="site-logo">
-                            <a class="brand" href="{{route('home') }}" title="Fabrice H Photographer" aria-label="Fabrice H Photographer">
-                                <img width="200" height="49" src="{{ asset('assets/images/logo.png') }}" class="neve-site-logo skip-lazy" alt="" decoding="async" loading="lazy" data-variant="logo" srcset="" sizes="(max-width: 200px) 100vw, 200px">
-                            </a>
+                    <div class="builder-item desktop-left">
+                        <div class="item--inner builder-item--logo" >
+                            <div class="site-logo">
+                                <a class="brand" href="{{route('home') }}" title="Fabrice H Photographer" aria-label="Fabrice H Photographer">
+                                    <img width="200" height="49" src="{{ asset('assets/images/logo.png') }}" class="neve-site-logo skip-lazy" alt="" decoding="async" loading="lazy" data-variant="logo" srcset="" sizes="(max-width: 200px) 100vw, 200px">
+                                </a>
+                            </div>
                         </div>
-
 	                 </div>
                 </div>
             </div>
             <div class="row">
            
-				
-				
-				
-				
+			
 				<a href="#">
                     <span class="et_pb_image_wrap ">
                         <img  width="1600" height="533" src="{{ asset('assets/images/DSC0389-Panorama_uxga.jpg') }}" >
