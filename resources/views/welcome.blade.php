00551cc99a954b3012366070dc09779a10b62fd6
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-05-15 12:34:33 +0400

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+            <meta charset="utf-8">
+            <meta name="csrf-token" content="{{ csrf_token() }}">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+            <meta name="description" content="Vitrine web du photographe Fabrice H">
+            <meta name="author" content="">
+            <meta name="keywords" content="art,photo,voyage,photographe,">
+            <meta name="robots" content="all">
+            <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+
+        <title>Fabrice H Photographer</title>
+
+        <!-- Fonts -->
+      
+        <link rel="stylesheet" href="{{asset('css/style.css') }}">
+  
+    </head>
+    <body class="antialiased">
+    <div class="container">
+			<div class="row row--wrapper" data-section="hfg_header_layout_main">
+				<div class="hfg-slot left"></div><div class="hfg-slot center"><div class="builder-item desktop-left"><div class="item--inner builder-item--logo" data-section="title_tagline" data-item-id="logo">
+	
+            <div class="site-logo">
+	                    <a class="brand" href="https://www.fabriceh-photographer.com/" title="Fabrice H Photographer" aria-label="Fabrice H Photographer"><img width="200" height="49" src="./Fabrice H Photographer_files/cropped-logo-collins-330.png" class="neve-site-logo skip-lazy" alt="" decoding="async" loading="lazy" data-variant="logo" srcset="" sizes="(max-width: 200px) 100vw, 200px"></a></div>
+
+	</div>
+
+</div></div><div class="hfg-slot right"></div>							</div>
+		</div>
+    </body>
+</html>
