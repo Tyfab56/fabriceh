cc12b05169cde9a16707eb514790b39d3306e0f9
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2023-09-04 15:16:27 +0400

    newsroom2

--- resources/views/frontend/bwr.blade.php
+++ resources/views/frontend/bwr.blade.php
@@ -53,3 +53,12 @@ The BWR Series benefits from a distinctive delivery method for 1/1 prints; they 
 </section>
 
 @endsection
+@push('scripts')
+<script type="text/javascript">
+var skill_barColor = "{{$SettingsData['color']->fill_color}}";
+var animatedColor = "{{$SettingsData['color']->theme_background_color}}";
+var home_page = "{{$SettingsData['home_page']}}";
+var isreCaptcha = "{{$SettingsData['recaptcha']}}";
+var skillsdata = <?php echo $skillsdata; ?>;
+</script>
+@endpush
