67a944ca033cfc67031829755fc5f830426d0b05
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2025-01-14 14:34:10 +0400

    fab

--- resources/views/frontend/coach.blade.php
+++ resources/views/frontend/coach.blade.php
@@ -5,13 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coaching Islande</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Chargement des fichiers CSS via Laravel Mix -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 
 <body>
     <div id="app">
         <island-coaching-hero></island-coaching-hero>
     </div>
+    <!-- Chargement des fichiers JS via Laravel Mix -->
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 
 </html>
