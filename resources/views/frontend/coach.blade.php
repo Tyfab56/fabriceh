26822c544a511ec3b06b0ceddee6ff66b7e9f2a5
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2025-01-14 12:12:09 +0400

    msm

--- resources/views/frontend/coach.blade.php
+++ resources/views/frontend/coach.blade.php
@@ -1,7 +1,3 @@
-@extends('layouts.app')
-
-@section('content')
 <div id="app">
-  <island-coaching-hero></island-coaching-hero>
-</div>
-@endsection
+    <island-coaching-hero></island-coaching-hero>
+</div>  
