aa32686e46b10ff11903dbcc8257e09031f987e4
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2025-01-14 13:10:49 +0400

    fab

--- resources/views/frontend/coach.blade.php
+++ resources/views/frontend/coach.blade.php
@@ -1,3 +1,17 @@
-<div id="app">
-    <island-coaching-hero></island-coaching-hero>
-</div>  
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coaching Islande</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div id="app">
+        <island-coaching-hero></island-coaching-hero>
+    </div>
+</body>
+
+</html>
