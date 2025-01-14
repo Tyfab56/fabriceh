28da9f795d012da606edf786dbf738a121e8ce0a
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2025-01-14 14:37:10 +0400

    fab

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,21 +6,14 @@
 
 require("./bootstrap");
 
-window.Vue = require("vue");
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
+import { createApp } from "vue";
 import IslandCoachingHero from "./components/IslandCoachingHero.vue";
 
-const app = createApp(App);
+// Créez l'application Vue
+const app = createApp({});
+
+// Enregistrez le composant globalement
 app.component("island-coaching-hero", IslandCoachingHero);
+
+// Montez l'application sur l'élément `#app`
 app.mount("#app");
